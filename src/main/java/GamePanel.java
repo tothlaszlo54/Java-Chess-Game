e2b1ebe23f667bcb5a78c8ee0c00614b7089357e
tothlaszlo54 <toth.laszlo.1516@gmail.com>
2024-11-13 21:02:41 +0100

    Gameloop created in the run method

--- src/main/java/GamePanel.java
+++ src/main/java/GamePanel.java
@@ -13,13 +13,32 @@ public class GamePanel extends JPanel implements Runnable {
         setBackground(Color.black);
     }
 
-    public void startGame(){
+    public void startGame() {
         gameThread = new Thread(this);
         gameThread.start();
     }
 
     @Override
     public void run() {
+
+        //Game loop
+        double drawInterval = 1000000000 / FPS;
+        double delta = 0;
+        long lastTime = System.nanoTime();
+        long currentTime;
+
+        while (gameThread != null) {
+            currentTime = System.nanoTime();
+
+            delta += (currentTime - lastTime) / drawInterval;
+            lastTime = currentTime;
+
+            if (delta >= 1) {
+                update();
+                repaint();
+                delta--;
+            }
+        }
 
     }
 
