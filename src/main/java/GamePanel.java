602a6b9aed1bf50f65a225605c78e887a925c91d
tothlaszlo54 <toth.laszlo.1516@gmail.com>
2024-11-13 20:18:47 +0100

    Create update method and override paintComponent and run method

--- src/main/java/GamePanel.java
+++ src/main/java/GamePanel.java
@@ -1,14 +1,30 @@
 import javax.swing.*;
 import java.awt.*;
 
-public class GamePanel extends JPanel {
+public class GamePanel extends JPanel implements Runnable{
 
     public static final int WIDTH = 1100;
     public static final int HEIGHT = 800;
+    final int FPS = 60;
+    Thread gameThread;
 
     public GamePanel (){
         setPreferredSize(new Dimension(WIDTH,HEIGHT));
         setBackground(Color.black);
     }
 
+    private void update(){
+
+    }
+
+    @Override
+    public void paintComponent(Graphics g){
+        super.paintComponent(g);
+
+    }
+
+    @Override
+    public void run() {
+
+    }
 }
