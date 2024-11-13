e39f7017653c24b2d8ab09ee86a1ab3b4ee0676c
tothlaszlo54 <toth.laszlo.1516@gmail.com>
2024-11-13 21:10:13 +0100

    create a Board class with variables and draw method

---
+++ src/main/java/Board.java
@@ -0,0 +1,13 @@
+import java.awt.*;
+
+public class Board {
+
+    final int MAX_COL = 8;
+    final int MAX_ROW = 8;
+    public static final int SQUARE_SIZE = 100;
+    public static final int HALF_SQUARE_SIZE = SQUARE_SIZE/2;
+
+    public void draw(Graphics2D g2){
+
+    }
+}
