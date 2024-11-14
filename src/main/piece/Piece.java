666f5093d684489ec13e92f3f7e53eac31a3f940
tothlaszlo54 <toth.laszlo.1516@gmail.com>
2024-11-14 19:38:08 +0100

    getImage, getX and getY method have created in Piece class

--- src/main/piece/Piece.java
+++ src/main/piece/Piece.java
@@ -1,16 +1,39 @@
+import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
+import java.io.IOException;
 
 public class Piece {
     public BufferedImage image;
-    public int x,y;
-    public int col,row,preCol,preRow;
+    public int x, y;
+    public int col, row, preCol, preRow;
     public int color;
 
-    public Piece(int color, int col, int row){
-        this.color=color;
-        this.col=col;
-        this.row=row;
+    public Piece(int color, int col, int row) {
+        this.color = color;
+        this.col = col;
+        this.row = row;
+        x = getX(col);
+        y = getY(row);
+        preCol=col;
+        preRow=row;
 
     }
 
+    public BufferedImage getImage(String imagePath){
+        BufferedImage image = null;
+
+        try {
+            image = ImageIO.read(getClass().getResourceAsStream(imagePath + ".png"));
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+        return image;
+    }
+    public int getX(int col) {
+        return col * Board.SQUARE_SIZE;
+    }
+
+    public int getY(int row) {
+        return row * Board.SQUARE_SIZE;
+    }
 }
