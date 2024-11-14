2632275de71d53db8fc91d383278bd7d1ab534df
tothlaszlo54 <toth.laszlo.1516@gmail.com>
2024-11-14 18:09:23 +0100

    Piece images added and Piece class created

---
+++ src/main/piece/Piece.java
@@ -0,0 +1,15 @@
+import java.awt.image.BufferedImage;
+
+public class Piece {
+    public BufferedImage image;
+    public int x,y;
+    public int col,row,preCol,preRow;
+    public int color;
+
+    public Piece(int color, int col, int row){
+        this.color=color;
+        this.col=col;
+        this.row=row;
+    }
+
+}
