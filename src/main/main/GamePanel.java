05c54dc4a1588977168865e809adbf5ef35a4fd1
tothlaszlo54 <toth.laszlo.1516@gmail.com>
2024-11-17 11:01:32 +0100

    setPieces and copyPieces methods created

--- src/main/main/GamePanel.java
+++ src/main/main/GamePanel.java
@@ -1,5 +1,6 @@
 import javax.swing.*;
 import java.awt.*;
+import java.util.ArrayList;
 
 public class GamePanel extends JPanel implements Runnable {
 
@@ -10,6 +11,11 @@ public class GamePanel extends JPanel implements Runnable {
 
     Board board = new Board();
 
+    //Pieces
+
+    public static ArrayList<Piece> pieces = new ArrayList<>();
+    public static ArrayList<Piece> simPieces = new ArrayList<>();
+
     //Color
     public static final int WHITE =0;
     public static final int BLACK =1;
@@ -18,11 +24,60 @@ public class GamePanel extends JPanel implements Runnable {
     public GamePanel() {
         setPreferredSize(new Dimension(WIDTH, HEIGHT));
         setBackground(Color.black);
+
+        setPieces();
+        copyPieces(pieces,simPieces);
     }
 
     public void startGame() {
         gameThread = new Thread(this);
         gameThread.start();
+    }
+
+    public void setPieces(){
+
+        //White team
+        pieces.add(new Pawn(WHITE,0,6));
+        pieces.add(new Pawn(WHITE,1,6));
+        pieces.add(new Pawn(WHITE,2,6));
+        pieces.add(new Pawn(WHITE,3,6));
+        pieces.add(new Pawn(WHITE,4,6));
+        pieces.add(new Pawn(WHITE,5,6));
+        pieces.add(new Pawn(WHITE,6,6));
+        pieces.add(new Pawn(WHITE,7,6));
+        pieces.add(new Rook(WHITE,0,7));
+        pieces.add(new Rook(WHITE,7,7));
+        pieces.add(new Knight(WHITE,1,7));
+        pieces.add(new Knight(WHITE,6,7));
+        pieces.add(new Bishop(WHITE,2,7));
+        pieces.add(new Bishop(WHITE,5,7));
+        pieces.add(new Queen(WHITE,3,7));
+        pieces.add(new King(WHITE,4,7));
+
+        //Black team
+        pieces.add(new Pawn(BLACK,0,1));
+        pieces.add(new Pawn(BLACK,1,1));
+        pieces.add(new Pawn(BLACK,2,1));
+        pieces.add(new Pawn(BLACK,3,1));
+        pieces.add(new Pawn(BLACK,4,1));
+        pieces.add(new Pawn(BLACK,5,1));
+        pieces.add(new Pawn(BLACK,6,1));
+        pieces.add(new Pawn(BLACK,7,1));
+        pieces.add(new Rook(BLACK,0,0));
+        pieces.add(new Rook(BLACK,7,0));
+        pieces.add(new Knight(BLACK,1,0));
+        pieces.add(new Knight(BLACK,6,0));
+        pieces.add(new Bishop(BLACK,2,0));
+        pieces.add(new Bishop(BLACK,5,0));
+        pieces.add(new Queen(BLACK,3,0));
+        pieces.add(new King(BLACK,4,0));
+    }
+
+    private void copyPieces(ArrayList<Piece> source,ArrayList<Piece> target){
+        target.clear();
+        for (int i = 0; i < source.size(); i++) {
+            target.add(source.get(i));
+        }
     }
 
     @Override
