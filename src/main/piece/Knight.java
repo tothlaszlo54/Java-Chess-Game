ece4d9918bf53b714648f0b1d049360e511369e9
tothlaszlo54 <toth.laszlo.1516@gmail.com>
2024-12-08 08:09:50 +0100

    canMove method is created for Knight piece

--- src/main/piece/Knight.java
+++ src/main/piece/Knight.java
@@ -8,4 +8,20 @@ public class Knight extends Piece {
             image = getImage("/piece/Knight-piece-black");
         }
     }
+
+    @Override
+    public boolean canMove(int targetCol, int targetRow) {
+
+        if(isWithinBoard(targetCol,targetRow)){
+            //Knight can move, if its movement ratio of col and row is 1:2 or 2:1
+            if(Math.abs(targetCol-preCol) * Math.abs(targetRow-preRow) == 2){
+                if(isValidSquare(targetCol,targetRow)){
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
+
 }
